52033f396870ea949276f1171f3f94ea4fc5c83e
Rocky3112 <alomgirhossainrocky@gmail.com>
2024-01-01 23:03:10 +0600

    all setup & navBar style done

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import { AppBar, Container, Grid, Grow, Typography } from "@mui/material";
+import memories  from './images/memories.png'
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import useStyles from './styles'
+
+
+function App() {
+
+  const classes = useStyles();
+  return (
+    <Container maxWidth ="lg">
+      <AppBar className={classes.appbar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h3" align="center">
+          Memorises
+
+        </Typography>
+        <img className={classes.image} src={memories} alt="memories" height="60" />
+
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justifyContent='space-between' alignItems="stretch" spacing={3} >
+            <Grid item xs ={12} sm={7}>
+                    <Posts />
+            </Grid>
+            <Grid item xs ={12} sm={4}>
+                   <Form />
+            </Grid>
+
+
+          </Grid>
+        </Container>
+      </Grow>
+
+    </Container>
+  );
+}
+
+export default App;
